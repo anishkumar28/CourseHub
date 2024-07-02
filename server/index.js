95f0ce76eb776b4e7b0b64b2afecfb328604da6d
Anish Kumar <anishkumar6527@gmail.com>
2024-07-02 12:44:09 +0530

    init project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+require("./config/database").connect();
+
+// import route
+const user = require("./routes/user");
+
+// mount route
+app.use("/api/v1",user);
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server started suceessfully at ${PORT}`);
+})
