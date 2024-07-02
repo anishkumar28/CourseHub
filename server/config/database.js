71f06c68e2eaeddd15b3340789238715176bad8c
Anish Kumar <anishkumar6527@gmail.com>
2024-07-02 12:47:36 +0530

    database connection

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL,)
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => {
+        console.log("Error connecting to database");
+        console.error(error.message);
+        process.exit(1);
+    })
+
+    };
