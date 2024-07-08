1f49e024dd68f82637e842cc99be013d2c4ac9e9
Anish Kumar <anishkumar6527@gmail.com>
2024-07-08 12:55:59 +0530

    controller for reset password

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -48,11 +48,66 @@ exports.resetPasswordToken = async (req,res) => {
         console.log(error);
         return res.status(500).json({
             success: false,
-            message: "Something went wrong while resetting your password"
+            message: "Something went wrong while resetting your password",
         });
     }
 }
 
 
 // resetPassword
+exports.resetPassword = async (req, res) => {
+    try{
+        // fetch data
+        const {password, confirmPassword, token} = req.body;
 
+        //valdiation
+        if(password != confirmPassword){
+            return res.json({
+                success: false,
+                message: "Password does not match",
+            });
+        }
+        
+        // get user details from database using token 
+        const userDetails = await User.findOne({token: token});
+
+        // if no entry - invalid token
+        if(!userDetails) {
+            return res.json({
+                success: false,
+                message: "Invalid token",
+            })
+        }
+
+        // check for token expiration time
+        if( userDetails.resetPasswordExpires < Date.now() ) {
+            return res.json({
+                success: false,
+                message: "Link expired",
+            })
+
+        }
+        // hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // update password
+        await User.findOneAndUpdate(
+            {token: token},
+            {password: hashedPassword},
+            {new: true},
+        );
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "Password reset successfully",
+        });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while resetting your password",
+        });
+    }
+}
