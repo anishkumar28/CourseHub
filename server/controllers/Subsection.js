92fe7b00fc0696cffcd15f05e90948ed9abd3c17
Anish Kumar <anishkumar6527@gmail.com>
2024-07-12 19:23:20 +0530

    controller for updating subsection

--- server/controllers/Subsection.js
+++ server/controllers/Subsection.js
@@ -67,4 +67,54 @@ exports.createSubSection = async (req, res) => {
             error:error.message,
         })
     }
-}
+};
+
+
+
+//updateSubSection => handler function
+exports.updateSubSection = async (req, res) => {
+    try {
+      const { sectionId, subSectionId, title, description } = req.body
+      const subSection = await SubSection.findById(subSectionId)
+  
+      if (!subSection) {
+        return res.status(404).json({
+          success: false,
+          message: "SubSection not found",
+        })
+      }
+  
+      if (title !== undefined) {
+        subSection.title = title
+      }
+  
+      if (description !== undefined) {
+        subSection.description = description
+      }
+      if (req.files && req.files.videoFile !== undefined) {
+        const video = req.files.videoFile;
+        const uploadDetails = await uploadImageToCloudinary(
+          video,
+          process.env.FOLDER_NAME
+        )
+        subSection.videoUrl = uploadDetails.secure_url
+        subSection.timeDuration = `${uploadDetails.duration}`
+      }
+  
+      await subSection.save()
+
+      const updatedSection = await Section.findById(sectionId).populate("subSection")
+  
+      return res.json({
+        success: true,
+        data:updatedSection,
+        message: "Section updated successfully",
+      })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({
+        success: false,
+        message: "An error occurred while updating the section",
+      })
+    }
+  }
