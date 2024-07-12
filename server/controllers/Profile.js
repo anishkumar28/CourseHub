337e9e5e5b37a4502ae509ad49848744a0d75dbe
Anish Kumar <anishkumar6527@gmail.com>
2024-07-12 19:28:57 +0530

    controller for update display picture

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -94,3 +94,55 @@ exports.deleteAccount = async (req, res) => {
         .json({ success: false, message: "User Cannot be deleted successfully" })
     }
   };
+
+
+
+
+  exports.getAllUserDetails = async (req, res) => {
+    try {
+      const id = req.user.id
+      const userDetails = await User.findById(id)
+        .populate("additionalDetails")
+        .exec()
+      console.log(userDetails)
+      res.status(200).json({
+        success: true,
+        message: "User Data fetched successfully",
+        data: userDetails,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+  }
+  
+  exports.updateDisplayPicture = async (req, res) => {
+    try {
+      const displayPicture = req.files.displayPicture
+      const userId = req.user.id
+      const image = await uploadImageToCloudinary(
+        displayPicture,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
+      console.log(image)
+      const updatedProfile = await User.findByIdAndUpdate(
+        { _id: userId },
+        { image: image.secure_url },
+        { new: true }
+      )
+      res.send({
+        success: true,
+        message: `Image Updated successfully`,
+        data: updatedProfile,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+  }
