63f4428b1f901472e7dea33f9ca979cd78ac7e8c
Anish Kumar <anishkumar6527@gmail.com>
2024-07-22 00:04:16 +0530

    model for otp

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,34 +1,46 @@
 const mongoose = require("mongoose");
+const {mailSender} = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 
-const otpSchema = new mongoose.Schema({
-    email: {
-        type: String,
+const OTPSchema = new mongoose.Schema({
+    email:{
+        type:String,
         required: true,
     },
     otp: {
-        type: String,
-        required: true,
+        type:String,
+        required:true,
     },
     createdAt: {
-        type: Date,
-        default: Date.now(),
-        expires: 5*60,
+        type:Date,
+        default:Date.now(),
+        // expires: 5*60*60*60,
     }
 });
 
-async function sendVerificationEmail(email,otp){
+
+// FUNCTION FOR SENDING... EMAILS
+async function sendVerificationEmail(email, otp) {
     try{
-        const mailResponse = await mailSender(email, "Verification email from Coursehub", otp);
-    }
-    catch(error){
-        console.log("Error occured while sending emails",error);
+        const mailResponse = await mailSender(email,
+             "Verification EMAIL from CourseHub by-Anish",
+             emailTemplate(otp));
+        console.log("Email sended Successfully!! => ", mailResponse);
+    } catch(error) {
+        // console.error(error);
+        console.log("error while SENDING.. EMAIL", error);
         throw error;
     }
 }
 
-otpSchema.pre("save", async function(next){
-    await sendVerificationEmail(this.email,this.otp);
+OTPSchema.pre("save", async function(next) {
+    // Only send an email when a new document is created
+    if (this.isNew) {
+        await sendVerificationEmail(this.email, this.otp);
+    }
     next();
-});
+} )
 
-module.exports = mongoose.model("OTP", otpSchema);
+const OTP = mongoose.model("OTP", OTPSchema);
+
+module.exports = OTP;
