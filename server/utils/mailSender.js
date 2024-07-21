d40f4ac8d88a07995d626b7f38921a0fe285c5e9
Anish Kumar <anishkumar6527@gmail.com>
2024-07-22 00:14:55 +0530

    adding utilities

---
+++ server/utils/mailSender.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+exports.mailSender = async (email, title, body) => {
+    try{
+        let transporter = nodemailer.createTransport({
+            host : process.env.MAIL_HOST,
+            auth:{
+                pass : process.env.MAIL_PASS,
+                user : process.env.MAIL_USER,
+            }
+
+        })
+
+        let info = await transporter.sendMail({
+            from: 'CourseHub ||  by ANISH',
+            to:`${email}`,
+            subject: `${title}`,
+            html: `${body}`,
+        });
+        console.log(info);
+        return info;
+
+    } catch(error) {
+        // console.error(error);
+        console.log(error.message);
+    }
+};
